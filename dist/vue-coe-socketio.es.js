187a4f768392278e7022040f4170dc6f5b0bbbf2
Vinicius Azevedo <viniazvd@gmail.com>
2018-09-19 22:49:00 -0300

    build

--- dist/vue-coe-socketio.es.js
+++ dist/vue-coe-socketio.es.js
@@ -5,54 +5,54 @@ var index = {
     var options = arguments.length > 2 && arguments[2] !== undefined ? arguments[2] : {};
     if (!address || typeof address !== 'string') throw new Error('[vue-coe-websocket] cannot locate connection');
     var socket = io(address, options);
-    Vue.prototype.$socket = socket;
+    Vue.prototype.$socket = {};
+    var isSet = Reflect.setPrototypeOf(Vue.prototype.$socket, socket);
 
-    var addListeners = function addListeners() {
-      var _this = this;
+    if (isSet) {
+      var addListeners = function addListeners() {
+        var _this = this;
 
-      if (this.$options['socket']) {
-        var _this$$options$socket = this.$options.socket,
-            _this$$options$socket2 = _this$$options$socket.events,
-            events = _this$$options$socket2 === void 0 ? {} : _this$$options$socket2,
-            _this$$options$socket3 = _this$$options$socket.channel,
-            channel = _this$$options$socket3 === void 0 ? '' : _this$$options$socket3; // subscribe to a channel
+        if (this.$options['socket']) {
+          var channel = Reflect.get(this.$options.socket, 'channel');
+          var events = Reflect.get(this.$options.socket, 'events'); // subscribe to a channel
 
-        if (channel) socket.nsp = channel;
+          if (channel) socket.nsp = channel;
 
-        if (events) {
-          Object.keys(events).forEach(function (event) {
-            var callback = events[event].bind(_this);
-
-            _this.$socket.on(event, callback);
-          });
+          if (events) {
+            // callback = events[event]
+            Object.keys(events).forEach(function (event) {
+              return _this.$socket.on(event, events[event].bind(_this));
+            });
+          }
         }
-      }
-    };
+      };
 
-    var removeListeners = function removeListeners() {
-      var _this2 = this;
+      var removeListeners = function removeListeners() {
+        var _this2 = this;
 
-      if (this.$options['socket']) {
-        var _this$$options$socket4 = this.$options.socket.events,
-            events = _this$$options$socket4 === void 0 ? {} : _this$$options$socket4;
+        if (this.$options['socket']) {
+          var events = Reflect.get(this.$options.socket, 'events');
 
-        if (events) {
-          var callback = events[event].__binded;
+          if (events) {
+            var callback = events[event];
 
-          var removeListener = function removeListener(event) {
-            return _this2.$socket.off(event, callback);
-          };
+            var removeListener = function removeListener(event) {
+              return _this2.$socket.off(event, callback);
+            };
 
-          Object.keys(events).forEach(removeListener);
-          this.$socket.disconnect();
+            Object.keys(events).forEach(removeListener);
+            this.$socket.disconnect();
+          }
         }
-      }
-    };
+      };
 
-    Vue.mixin({
-      beforeCreate: addListeners,
-      beforeDestroy: removeListeners
-    });
+      Vue.mixin({
+        beforeCreate: addListeners,
+        beforeDestroy: removeListeners
+      });
+    } else {
+      console.error('[vue-coe-websocket] cannot set the prototype');
+    }
   }
 };
 
