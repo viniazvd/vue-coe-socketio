820c4c2f7b307a0f06d1b8185493697636bfb9f1
Vinicius Azevedo <viniazvd@gmail.com>
2018-09-19 23:23:03 -0300

    refactor #2

--- index.js
+++ index.js
@@ -6,10 +6,9 @@ export default {
 
     const socket = io(address, options)
 
-    Vue.prototype.$socket = {}
-    const isSet = Reflect.setPrototypeOf(Vue.prototype.$socket, socket)
+    const λ = Object.create(null)
 
-    if (isSet) {
+    if (Reflect.set(λ, '$socket', socket)) {
       const addListeners = function () {
         if (this.$options['socket']) {
           const channel = Reflect.get(this.$options.socket, 'channel')
@@ -20,7 +19,9 @@ export default {
 
           if (events) {
             // callback = events[event]
-            Object.keys(events).forEach(event => this.$socket.on(event, events[event].bind(this)))
+            const addListener = event => λ.$socket.on(event, events[event].bind(this))
+
+            Object.keys(events).forEach(addListener)
           }
         }
       }
@@ -31,19 +32,18 @@ export default {
 
           if (events) {
             const callback = events[event]
-            const removeListener = event => this.$socket.off(event, callback)
+            const removeListener = event => λ.$socket.off(event, callback)
 
             Object.keys(events).forEach(removeListener)
 
-            this.$socket.disconnect()
+            λ.$socket.disconnect()
           }
         }
       }
 
-      Vue.mixin({
-        beforeCreate: addListeners,
-        beforeDestroy: removeListeners
-      })
+      Vue.mixin({ beforeCreate: addListeners, beforeDestroy: removeListeners })
+
+      Object.defineProperty(Vue.prototype, '$socket', { get: () => λ.$socket })
     } else {
       console.error('[vue-coe-websocket] cannot set the prototype')
     }
